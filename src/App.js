022c05d08164aafb117623eff7ea7ea26ba1d02b
kapil jhajhria <jhajhria44@gmail.com>
2021-01-13 23:32:58 +0530

    if token is present, set user object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CrmHome, { makePostRequest } from "./CrmHome";
 import { Route, Switch, useHistory } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import EnquiryForm from "./EnquiryForm";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Button } from "@material-ui/core";
 import "react-toastify/dist/ReactToastify.css";
+import jwtDecode from "jwt-decode";
+import { setUser } from "@sentry/react";
 
 function App(props) {
   const history = useHistory();
-
+  const [user, setUser] = useState({});
   async function logOutUser() {
     console.log("logging out user");
     let result = await makePostRequest("http://localhost:5000/logout", {
@@ -25,6 +28,10 @@ function App(props) {
   }
 
   useEffect(() => {
+    const jwt = localStorage.getItem("token");
+    const user = jwtDecode(jwt);
+    console.log("user is", user);
+    setUser(user);
     const test = props;
     console.log("test:");
     console.log(test);
