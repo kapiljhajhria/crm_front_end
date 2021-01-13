ac23fe0e6a3d7a11c8c2d07b318e1bdd98aebe64
kapil jhajhria <jhajhria44@gmail.com>
2021-01-13 22:03:10 +0530

    loading sentry service for logging errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import logger from "./services/logService";
+
+logger.init();
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <App/>
-        </Router>
-    </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
