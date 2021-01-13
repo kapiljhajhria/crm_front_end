30041cd5878756959ae77f9d726ab809314eea19
kapil jhajhria <jhajhria44@gmail.com>
2021-01-14 01:24:46 +0530

    customerService created

---
+++ src/services/customerService.js
@@ -0,0 +1,20 @@
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+
+const apiEndpoint = apiUrl + "/customers";
+
+function customerUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
+export function getCustomers() {
+  return http.get(apiEndpoint);
+}
+
+export function saveCustomer(customer) {
+  return http.post(apiEndpoint, customer);
+}
+
+export function deleteCustomer(movieId) {
+  return http.delete(customerUrl(movieId));
+}
