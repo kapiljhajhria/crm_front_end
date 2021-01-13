33071a2dc8bfd9bc61d195a4490a6d61e759a65c
kapil jhajhria <jhajhria44@gmail.com>
2021-01-14 01:02:36 +0530

    protectedRoute component created and used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import CrmHome, { makePostRequest } from "./components/CrmHome";
+import ProtectedRoute from "./components/common/protectedRoute";
 import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import EnquiryForm from "./components/EnquiryForm";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,6 +10,7 @@ import { Button } from "@material-ui/core";
 import "react-toastify/dist/ReactToastify.css";
 import LogOut from "./components/logout";
 import authService from "./services/authService";
+
 function App(props) {
   const history = useHistory();
   const [user, setUser] = useState({});
@@ -41,14 +43,7 @@ function App(props) {
       </AppBar>
       <header className="App-header">
         <Switch>
-          <Route
-            exact
-            path={"/customers"}
-            render={(props) => {
-              if (!user) return <Redirect to="/" />;
-              return <EnquiryForm {...props} />;
-            }}
-          />
+          <ProtectedRoute exact path={"/customers"} component={EnquiryForm} />
 
           <Route path={"/"} component={CrmHome} />
           <Route path={"/logout"} component={LogOut} />
---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import authService from "../../services/authService";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+  return (
+    <Route
+      exact
+      {...rest}
+      render={(props) => {
+        if (!authService.getCurrentUser()) return <Redirect to="/" />;
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
+  );
+};
+export default ProtectedRoute;
