5c0bd882f2577bfee31e8407556dd107e15d7b84
kapil jhajhria <jhajhria44@gmail.com>
2021-01-14 02:22:00 +0530

    Updates

---
+++ precache-manifest.e588ccd7ba1800d85c3a9e25b0271d93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49da969b825ef267a04ae687d3eccc1f",
+    "url": "/crm_front_end/index.html"
+  },
+  {
+    "revision": "59e1c5752d7bb191f6e3",
+    "url": "/crm_front_end/static/css/2.1781c263.chunk.css"
+  },
+  {
+    "revision": "924f52617bb9d676283a",
+    "url": "/crm_front_end/static/css/main.dc1a42c6.chunk.css"
+  },
+  {
+    "revision": "59e1c5752d7bb191f6e3",
+    "url": "/crm_front_end/static/js/2.f8cfb8a7.chunk.js"
+  },
+  {
+    "revision": "149a5a21a0384d0aaf10c5ceda3815c3",
+    "url": "/crm_front_end/static/js/2.f8cfb8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924f52617bb9d676283a",
+    "url": "/crm_front_end/static/js/main.1680baa2.chunk.js"
+  },
+  {
+    "revision": "5741de4b091dbec55aa2",
+    "url": "/crm_front_end/static/js/runtime-main.668569b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactForms/precache-manifest.61aa093efb341f3a65cdab5f1a7ab90a.js"
+  "/crm_front_end/precache-manifest.e588ccd7ba1800d85c3a9e25b0271d93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactForms/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crm_front_end/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
