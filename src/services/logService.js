1d014110081ee5431d226d11bda6dde5fcc1121b
kapil jhajhria <jhajhria44@gmail.com>
2021-01-13 22:37:58 +0530

    loggerService created

---
+++ src/services/logService.js
@@ -0,0 +1,24 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn:
+      "https://ce625a42c479474d86a00952b0109351@o504500.ingest.sentry.io/5591499",
+    autoSessionTracking: true,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
