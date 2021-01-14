829cd1769361ffba4294e4eef8a1fd64babfb7b2
kapil jhajhria <jhajhria44@gmail.com>
2021-01-15 00:37:02 +0530

    don't reload on logout

--- src/components/logout.js
+++ src/components/logout.js
@@ -3,7 +3,6 @@ import React, { Component, useEffect } from "react";
 export default function LogOut() {
   useEffect(() => {
     localStorage.removeItem("token");
-    window.location = "/";
   }, []);
   return null;
 }
